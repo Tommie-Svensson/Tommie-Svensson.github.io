29807f6981d47cd609c76e23abc87d226a9dc3ad
Tommie Svensson <jan.tommie.svensson@gmail.com>
2018-02-26 17:48:51 +0100

    6

--- maxi.js
+++ maxi.js
@@ -15,7 +15,7 @@
             method: "PUT",
             cache: false,
             url: url,
-            data: JSON.stringify({"on":true}),
+            data: JSON.stringify({"on":true,"hue":10000}),
             contentType: "text/plain",
             success: function () {
                 callback();
@@ -46,11 +46,31 @@
         });
     };
 
+    ext.lampGreen = function() {
+        // Make an AJAX call to the Open Weather Maps API
+        var url = 'http://192.168.1.61/api/DLm-zlYP-nABkiO7iFLqAcoyTeuxk-EooFBj7EAO/lights/8/state';
+         $.ajax({
+            method: "PUT",
+            cache: false,
+            url: url,
+            data: JSON.stringify({"on":false,"hue":64921,"ct":153}),
+            contentType: "text/plain",
+            success: function () {
+                callback();
+            },
+            error: function (xhr, textStatus, error) {
+                console.log(error);
+                callback();
+            }
+        });
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             [' ', 'Tänd', 'lampOn'],
             [' ', 'Släck', 'lampOff'],
+            [' ', 'Grön', 'lampGreen'],
         ]
     };
 
