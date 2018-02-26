ed270ca0642af55ed2c5e4c0ac336659666168b5
Tommie Svensson <jan.tommie.svensson@gmail.com>
2018-02-26 19:14:03 +0100

    test

--- maxi.js
+++ maxi.js
@@ -52,13 +52,13 @@
 
     ext.lampColor = function(hue, sat, bri, callback) {
         // Make an AJAX call to the Open Weather Maps API
-        var json = '"on":true,"hue":' + hue + ',"sat":'+ sat + ',"bri":' + bri + '';
+        var json = '{"on":true,"hue":' + hue + ',"sat":'+ sat + ',"bri":' + bri + '}';
         var url = 'http://192.168.1.61/api/DLm-zlYP-nABkiO7iFLqAcoyTeuxk-EooFBj7EAO/lights/8/state';
          $.ajax({
             method: "PUT",
             cache: false,
             url: url,
-            data: JSON.stringify({json}),
+            data: JSON.stringify(json),
             contentType: "text/plain",
             success: function () {
                 callback();
