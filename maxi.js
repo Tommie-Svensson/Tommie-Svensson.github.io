fa04393fc6049965558a362443989b67fc538c95
Tommie Svensson <jan.tommie.svensson@gmail.com>
2018-02-26 15:39:06 +0100

    first

---
+++ maxi.js
@@ -0,0 +1,31 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+
+    ext.lampOn = function() {
+        // Make an AJAX call to the Open Weather Maps API
+        $.ajax({
+              url: 'http://192.168.1.61/api/DLm-zlYP-nABkiO7iFLqAcoyTeuxk-EooFBj7EAO/lights/8/state',
+              type: 'PUT',
+              data: '{"on":true}',
+              dataType: 'jsonp',
+              success: callback
+        });
+    };
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            [' ', 'Maxi', 'lampOn'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Weather extension', descriptor, ext);
+})({});
